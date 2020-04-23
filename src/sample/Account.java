e68e3a2b2f5d3979fdb81e52a47893a84d22e604
Valter Sjöstrand <Valter.sjo@gmail.com>
2020-04-23 19:45:06 +0200

    fixa account o crime

--- src/sample/Account.java
+++ src/sample/Account.java
@@ -1,4 +1,21 @@
 package sample;
 
 public class Account {
+    private Person owner;
+    private String username;
+    private int password;
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+
 }
--- src/sample/Crime.java
+++ src/sample/Crime.java
@@ -1,4 +1,33 @@
 package sample;
 
+import java.util.Date;
+
 public class Crime {
+    private Date dateOfCrime;
+    private Enum typeOfCrime;
+    private Person suspect;
+    private CrimeRapport rapport;
+
+    public Crime(Date dateOfCrime, Enum typeOfCrime, Person suspect, CrimeRapport rapport) {
+        this.dateOfCrime = dateOfCrime;
+        this.typeOfCrime = typeOfCrime;
+        this.suspect = suspect;
+        this.rapport = rapport;
+    }
+
+    public Date getDateOfCrime() {
+        return dateOfCrime;
+    }
+
+    public Enum getTypeOfCrime() {
+        return typeOfCrime;
+    }
+
+    public Person getSuspect() {
+        return suspect;
+    }
+
+    public CrimeRapport getRapport() {
+        return rapport;
+    }
 }
