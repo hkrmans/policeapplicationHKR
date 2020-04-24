b401b55c4fa1e0a7c2173952bbaf821a691eabb3
Mats Lundager <51341536+B0gg3@users.noreply.github.com>
2020-04-24 09:06:19 +0200

    Update DbConnect.java

--- src/sample/DbConnect.java
+++ src/sample/DbConnect.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class DbConnect {
     public Connection connect(String username,String password) throws SQLException {
-        Connection connect = DriverManager.getConnection("/usr/share/java/mysql-connector-java-8.0.19.jar",username,password);
+        Connection connect = DriverManager.getConnection("den1.mysql3.gear.host",username,password);
         return connect;
     }
 }
