7ebec592c80ef45e609755f5e9eefd4b3ead846d
B0gg3 <mats_06_20@hotmail.com>
2020-05-04 15:35:51 +0200

    Db fix etc

--- src/sample/DbConnect.java
+++ src/sample/DbConnect.java
@@ -1,17 +1,28 @@
-// Not fully functioning.
+package sample;
+
 import java.sql.*;
+import java.util.*;
 
-public class DbConnection {
-    public Connection connect(String username, String password) {
-        try {
-            Connection connect = DriverManager.getConnection("den1.mysql3.gear.host", username, password);
-            return connect;
-        } catch (SQLException E) {
+class DbConnect{
+    private static DbConnect single_instance = null;
+    private static Connection connection;
+
+    DbConnect(String username, String password) throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:mysql://den1.mysql3.gear.host",username,password);
+    }
+
+    public static Connection getInstance(){
+        return connection;
+    }
+
+    public ArrayList<Person> searchDb() throws SQLException {
+        Statement stmt = connection.createStatement();
+        stmt.executeQuery("use policemanagment");
+        ResultSet rs = stmt.executeQuery("select * from person");
+        ArrayList<Person> personList = new ArrayList<>();
+        while(rs.next()){
+            Civilian person = new Civilian(rs.getString(1),rs.getString(2),rs.getString(3));
         }
-        return null;
+        return personList;
     }
-    public void search(Connection connect,String statement) throws SQLException {
-        Statement stmt = connect.createStatement();
-    }
-
 }
