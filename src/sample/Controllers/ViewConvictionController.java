937198d149a64f08f9437db57ac0880b9110e906
Valter Sjöstrand <Valter.sjo@gmail.com>
2020-05-06 16:54:06 +0200

    Minor update conviction controller

--- src/sample/Controllers/ViewConvictionController.java
+++ src/sample/Controllers/ViewConvictionController.java
@@ -2,15 +2,42 @@ package sample.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import sample.Conviction;
 import sample.SceneChanger;
+import sample.Singleton;
+
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class ViewConvictionController {
+public class ViewConvictionController implements Initializable {
+    private ArrayList<Conviction> convictions;
+
+    @FXML
+    private TextArea convictionsArea;
 
     @FXML
     void LogOutViewConvictionButtonOnAction(ActionEvent event) throws IOException {
         SceneChanger.changeScene(event, "sample.fxml");
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            convictions = Singleton.getConvictionInstance().getConvictionList();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        for (Conviction c : convictions) {
+            convictionsArea.appendText(c.getPrisoner() + " | " + c.getSentence() + " | "
+                                        + c.getConviction() + "|" + c.getRelease());
+        }
+
 
     }
 }
